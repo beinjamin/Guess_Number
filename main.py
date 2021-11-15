5aa0b48bfda3b3cacdb3c0199eda72f71acb0c1f
beinjamin <jauresbeinjamin@gmail.com>
2021-11-15 13:04:37 +0100

    Gestion de la condiction if

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import random
-print(random.randint(1, 50))
+nombreMagique = random.randint(1, 50)
 
 for i in range (0, 4):
 
     nombreEntre = input ("Bonjour Monsieur Merci de deviner un nombre")
+
+
+    if nombreEntre > nombreMagique:
+
+        print ("Votre nombre que vous avez entrer est superieur au nombre magique")
+
+    if nombreEntre < nombreMagique:
+        print("Votre nombre que vous avez entrer est inferieux au nombre magique")
